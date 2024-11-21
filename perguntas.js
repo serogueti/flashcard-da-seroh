6edf9111e67765cdad87ef38eb00dc3e6370904d
serogueti <00001108745167sp@al.educacao.sp.gov.br>
2024-11-21 07:44:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'natureza',
+    'como as flores se pigmentam?',
+    'Pigmentos nascem nessas plantas, produzindo uma gama de cores através do espectr0',
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de Paris?',
+    'A capital de Paris é a França'
+)
+
+criaCartao(
+    'Pintura',
+    'quem pintou a Monalisa?',
+    'Leonardo da Vinci '
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz amor em Inglês?',
+    'amor em ingles é  love',
+)
